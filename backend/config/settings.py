b4cf3b180323ef78a9d27c860de49e93aa6f3efe
muhammadjalilov <mjalilov2004@gmail.com>
2024-11-29 15:05:10 +0500

    added port 3000

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -151,6 +151,7 @@ SPECTACULAR_SETTINGS = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:9000",
+    "http://127.0.0.1:3000",
     "http://127.0.0.1:8000",
     "http://127.0.0.1:8002",
 ]
